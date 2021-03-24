9a8c44ed41e36ff6e0e7a1d6558e34e3e5ef3105
Mohammad Sadegh Rasooli <rasooli.ms@gmail.com>
2021-03-24 12:08:45 -0400

    Update extract_projections_from_mst_to_conllu.py

--- extract_projections_from_mst_to_conllu.py
+++ extract_projections_from_mst_to_conllu.py
@@ -26,16 +26,33 @@ print("Loading Conllu trees!")
 conllu_trees = open(os.path.abspath(sys.argv[2]), "r").read().strip().split("\n\n")
 print("Conllu trees loading done with {0} trees!".format(len(conllu_trees)))
 
-found = 0
-for ti, tree in enumerate(conllu_trees):
-    word_and_ids = list(map(lambda line: line.strip().split("\t")[:2], tree.strip().split("\n")))
-    sen = " ".join(none_filter(map(lambda word_id: word_id[1] if word_id[0].isdigit() else None, word_and_ids)))
+with open(os.path.abspath(sys.argv[3]), "w") as writer:
+    found = 0
+    for ti, tree in enumerate(conllu_trees):
+        lines = tree.strip().split("\n")
+        word_and_ids = list(map(lambda line: line.strip().split("\t")[:2], lines))
+        sen = " ".join(none_filter(map(lambda word_id: word_id[1] if word_id[0].isdigit() else None, word_and_ids)))
 
-    if sen in sentence_trees:
-        found +=1
+        if sen in sentence_trees:
+            found +=1
+            output = []
+            labels, relations = sentence_trees[sen]
 
-    if ti%10000==0:
-        print(ti, "found:", found, end="\r")
+            for line in lines:
+                spl = line.strip().split("\t")
+                if spl[0].isdigit():
+                    wid = int(spl[0])-1
+                    rel = relations[wid]
+                    dep_labl = labels[wid]
+
+                    spl[6] = rel
+                    spl[7] = dep_labl
+                writer.write("\t".join(spl))
+                writer.write("\n")
+            writer.write("\n")
+
+        if ti%10000==0:
+            print(ti, "found:", found, end="\r")
 
 print("{0} Conllu trees found in MST trees!".format(found))
 
