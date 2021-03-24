3c858cd630ee36c59943a3b93eb9435b0f7b569d
Mohammad Sadegh Rasooli <rasooli.ms@gmail.com>
2021-03-24 11:06:33 -0400

    Create extract_projections_from_mst_to_conllu.py

---
+++ extract_projections_from_mst_to_conllu.py
@@ -0,0 +1,21 @@
+import os
+import sys
+
+
+print("Loading MST trees!")
+sentence_trees = {}
+with open(os.path.abspath(sys.argv[1]), "r") as mst_reader:
+    sentence = []
+    for line in mst_reader:
+        if len(line.strip()) == 0 and len(sentence)>0:
+            sentence = " ".join(sentence[0])
+            labels = sentence[2]
+            relations = sentence[3]
+            sentence_trees[sentence] = (labels, relations)
+            sentence = []
+        else:
+            sentence.append(line.strip().split("\t")) 
+
+
+print("MST trees loading done with {1} trees!".format(len(sentence_trees)))
+
