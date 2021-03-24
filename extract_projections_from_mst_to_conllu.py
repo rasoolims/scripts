9671a1cf260758668f6e7d5b3f2a51f9723aa8b6
Mohammad Sadegh Rasooli <rasooli.ms@gmail.com>
2021-03-24 11:52:15 -0400

    Update extract_projections_from_mst_to_conllu.py

--- extract_projections_from_mst_to_conllu.py
+++ extract_projections_from_mst_to_conllu.py
@@ -1,6 +1,7 @@
 import os
 import sys
 
+none_filter = lambda x: filter(lambda x: x is not None, x)
 
 print("Loading MST trees!")
 sentence_trees = {}
@@ -19,7 +20,20 @@ with open(os.path.abspath(sys.argv[1]), "r") as mst_reader:
 
 print("MST trees loading done with {0} trees!".format(len(sentence_trees)))
 
+
 print("Loading Conllu trees!")
 
 conllu_trees = open(os.path.abspath(sys.argv[2]), "r").read().strip().split("\n\n")
-print("MST trees loading done with {0} trees!".format(len(conllu_trees)))
+print("Conllu trees loading done with {0} trees!".format(len(conllu_trees)))
+
+found = 0
+for tree in conllu_trees:
+    word_and_ids = list(map(lambda line: line.strip().split("\t")[:2], tree.strip().split("\n")))
+    sen = " ".join(none_filter(map(lambda word_id: word_id[1] if word_id[0].isdigit() else None, word_and_ids)))
+
+    if sen in sentence_trees:
+        found +=1
+
+print("{0} Conllu trees found in MST trees!".format(found))
+
+
