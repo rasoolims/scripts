c1875c09862122156c3fd7f52f750e720918b1e8
Mohammad Sadegh Rasooli <rasooli.ms@gmail.com>
2021-03-30 16:48:54 -0400

    Update extract_eu_parallel_from_translations.py

--- extract_eu_parallel_from_translations.py
+++ extract_eu_parallel_from_translations.py
@@ -4,6 +4,9 @@ import sys
 
 en_path = os.path.abspath(sys.argv[1])
 en_tok_path = os.path.abspath(sys.argv[2])
+foreign_path = os.path.abspath(sys.argv[3])
+en_foreign_path = os.path.abspath(sys.argv[4])
+foreign_tok_path = os.path.abspath(sys.argv[5])
 
 en_raw2tok = {}
 
@@ -11,4 +14,29 @@ with open(en_path, "r") as en_reader, open(en_tok_path, "r") as en_tok_reader:
 	for line, tok_line in zip(en_reader, en_tok_reader):
 		en_raw2tok[line.strip()] = tok_line.strip()
 print(len(en_raw2tok))
+
+
+
+raw_foreign_sens = []
+tok_foreign_sens = []
+en_equiv_sens = []
+
+with open(foreign_path, "r") as foreign_reader, open(en_foreign_path, "r") as en_foreign_reader:
+	for fl, el in zip(foreign_reader, en_foreign_reader):
+		fl = fl.strip()
+		if len(fl)>0:
+			raw_foreign_sens.append(fl)
+			en_equiv_sens.append(el)
+
+print(len(raw_foreign_sens))
+
+with open(foreign_tok_path, "r") as foreign_tok_reader:
+	for fl in foreign_tok_reader:
+		fl = fl.strip()
+		if len(fl)>0:
+			tok_foreign_sens.append(fl)
+
+print(len(tok_foreign_sens))
+
+assert len(raw_foreign_sens) == len(tok_foreign_sens)
 
