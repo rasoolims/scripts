2331f7c09297ab1a92c31e4e380d67def2995355
Mohammad Sadegh Rasooli <rasooli.ms@gmail.com>
2021-03-30 17:41:26 -0400

    Update extract_eu_parallel_from_translations.py

--- extract_eu_parallel_from_translations.py
+++ extract_eu_parallel_from_translations.py
@@ -10,6 +10,8 @@ en_foreign_path = os.path.abspath(sys.argv[5])
 foreign_tok_path = os.path.abspath(sys.argv[6])
 en_other_fastalign =  os.path.abspath(sys.argv[7])
 other_tokenized =  os.path.abspath(sys.argv[8])
+output_file_source =  os.path.abspath(sys.argv[9])
+output_file_target =  os.path.abspath(sys.argv[10])
 
 en_raw2tok = {}
 en_moses2raw = {}
@@ -17,7 +19,7 @@ en_moses2raw = {}
 with open(en_path, "r") as en_reader, open(en_tok_path, "r") as en_tok_reader, open(en_moses_path, "r") as en_moses_reader:
 	for line, tok_line, moses_line in zip(en_reader, en_tok_reader, en_moses_reader):
 		en_raw2tok[line.strip()] = tok_line.strip()
-		en_moses2raw[moses_line.strip()] = line.strip()
+		en_moses2raw[moses_line.strip()] = tok_line.strip()
 
 print(len(en_raw2tok), len(en_moses2raw))
 
@@ -27,6 +29,8 @@ raw_foreign_sens = []
 tok_foreign_sens = []
 en_equiv_sens = []
 missed_tokenized = 0
+
+en2foreign_tok = {}
 
 with open(foreign_path, "r") as foreign_reader, open(en_foreign_path, "r") as en_foreign_reader:
 	for fl, el in zip(foreign_reader, en_foreign_reader):
@@ -47,7 +51,9 @@ with open(foreign_tok_path, "r") as foreign_tok_reader:
 	for fl in foreign_tok_reader:
 		fl = fl.strip()
 		if len(fl)>0:
+			en2foreign_tok[en_equiv_sens[len(tok_foreign_sens)]] = fl
 			tok_foreign_sens.append(fl)
+
 
 print(len(tok_foreign_sens))
 
@@ -81,4 +87,18 @@ with open(other_tokenized, "r") as other_tokenized_reader:
 
 print(len(translations_tok))
 assert len(translations) == len(translations_tok)
+
+written = 0
+with open(output_file_source, "w") as source_writer, open(output_file_target, "w") as target_writer:
+	for i, en_sen in enumerate(en_to_translate):
+		en_tok_sen = en_moses2raw[en_sen] if en_sen in en_moses2raw else en_sen
+		translation = translations_tok[i]
+		if en_tok_sen in en2foreign_tok:
+			source_writer.write(en2foreign_tok[en_tok_sen] + "\n")
+			target_writer.write(translation + "\n")
+			written += 1
+print("written", written, "parallel sentences!")
+
+
+
 
