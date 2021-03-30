2eb66bf180a7bb9240c61e517d497d157af91c6a
Mohammad Sadegh Rasooli <rasooli.ms@gmail.com>
2021-03-30 16:03:29 -0400

    Create conllu2raw_tok.py

---
+++ conllu2raw_tok.py
@@ -0,0 +1,15 @@
+import os
+import sys
+
+with open(os.path.abspath(sys.argv[1]), "r") as reader, open(os.path.abspath(sys.argv[2]), "w") as writer:
+	words = []
+	for line in reader:
+		line = line.strip()
+		if len(line) == 0 and len(words)>0:
+			writer.write(" ".join(words)+"\n")
+			words = []
+
+		spl = line.split("\t")[0]
+		if spl.isdigit():
+			words.append(spl[1].strip())
+			
